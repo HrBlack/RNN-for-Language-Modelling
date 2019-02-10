684ea0696af131e0edfe6d232750852e4b783842
liushihao1996@yeah.net <liushihao1996@yeah.net>
2019-02-10 20:59:43 +0000

    d

--- lstm_gru_trail.py
+++ lstm_gru_trail.py
@@ -1,18 +1,6 @@
 to #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  3 16:41:31 2019
 
-@author: s1583620
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  3 16:31:56 2019
-
-@author: s1583620
-"""
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
@@ -25,61 +13,8 @@ import matplotlib.pyplot as plt
 
 from utils import *
 from rnnmath import *
-from sys import stdout
+
 torch.manual_seed(1)
-
-data_folder = '/afs/inf.ed.ac.uk/user/s15/s1583620/nlu-coursework/data'
-
-train_size = 25000
-dev_size = 1000
-vocab_size = 2000
-hdim = 50
-Max_epochs = 30
-
-vocab = pd.read_table(data_folder + "/vocab.wiki.txt", header=None, sep="\s+", index_col=0, names=['count', 'freq'], )
-num_to_word = dict(enumerate(vocab.index[:vocab_size]))     #Mapping from index to word
-word_to_num = invert_dict(num_to_word)      # Mapping from word to index
-
-docs = load_lm_dataset(data_folder + '/wiki-train.txt')
-S_train = docs_to_indices(docs, word_to_num, 1, 1)
-X_train, D_train = seqs_to_lmXY(S_train)
-
-docs = load_lm_dataset(data_folder + '/wiki-dev.txt')
-S_dev = docs_to_indices(docs, word_to_num, 1, 1)
-X_dev, D_dev = seqs_to_lmXY(S_dev)
-
-X_train = X_train[:train_size]
-D_train = D_train[:train_size]      # Targets, i.e. one-offset X_train
-X_dev = X_dev[:dev_size]
-D_dev = D_dev[:dev_size]
-
-
-
-
-
-
-def batchify(data,batch_size):
-    """
-    Batchify the data, input should come from X_Train[:batch_size], i.e. a list
-    of size batch_size. In the list are arrays.
-    """
-    n_batch = len(data)//batch_size   # Number of batches
-    data = data[:n_batch*batch_size]    # Discard the extra data
-    #====Till now, data is still a list
-    tensor_data = torch.zeros()
-    for i in range(len(data)):
-        pass
-    
-
-def data_to_tensor(seq,batch_size = 1):
-    """
-    Input should be one sentence, e.g. X_train[0]
-    Output will be an variable tensor
-    """
-    tensor = torch.LongTensor(seq)
-    return autograd.Variable(tensor)
-
-
 
 
 class RNNTagger(nn.Module): 
@@ -88,7 +23,7 @@ class RNNTagger(nn.Module):
         self.hidden_dim = hidden_dim
         self.vocab_size = vocab_size
         
-        #self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        # self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
         
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
@@ -121,15 +56,13 @@ class RNNTagger(nn.Module):
         return tag_scores
 
 
-
 class LSTMTagger(nn.Module):
-    
     def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
         super(LSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
         self.vocab_size = vocab_size
         
-        #self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        # self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
         
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
@@ -169,7 +102,7 @@ class GRUTagger(nn.Module):
         self.hidden_dim = hidden_dim
         self.vocab_size = vocab_size
         
-        #self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        # self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
         
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
@@ -188,196 +121,162 @@ class GRUTagger(nn.Module):
         return one_hot
     
     def init_hidden(self):
-        # Before we've done anything, we dont have any hidden state.
-        # Refer to the Pytorch documentation to see exactly why they have this dimensionality.
-        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
+        """
+        Before we've done anything, we don't have any hidden states.
+        Refer to the Pytorch documentation to see exactly why they have this dimensionality.
+        The axes semantics are (num_layers, minibatch_size, hidden_dim)
+        """
         return (autograd.Variable(torch.zeros(1, 1, self.hidden_dim)))
         
     def forward(self, sentence):
-        #embeds = self.word_embeddings(sentence)
         embeds = self.one_hot(sentence)
         lstm_out, self.hidden = self.lstm(embeds.view(len(sentence), 1, -1), self.hidden)
         tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
         tag_scores = F.log_softmax(tag_space)
         return tag_scores
 
-f = open('result.txt','w')
 
+def train(model, loss_function, optimizer):
+    loss_list = []
+    loss_dev_list = []
+    t0 = time.time()
+    for epoch in range(Max_epochs):
+        loss_sum = 0
+        loss_dev = 0
+        for idx in range(train_size):
+            X_tensor = data_to_tensor(X_train[idx])
+            Y_tensor = data_to_tensor(D_train[idx])
+            model_rnn.zero_grad()
+            model_rnn.hidden = model_rnn.init_hidden()
+
+            pred_scores = model_rnn(X_tensor)
+            loss = loss_function(pred_scores, Y_tensor)
+            loss.backward()
+            optimizer.step()
+
+            loss_sum += np.array(loss.data)
+        loss_list.append(loss_sum / train_size)
+
+        if epoch % 2 == 0:
+            for idx in range(dev_size):
+                X_tensor = data_to_tensor(X_dev[idx])
+                Y_tensor = data_to_tensor(D_dev[idx])
+                pred_dev = model(X_tensor)
+                loss_d = loss_function(pred_dev, Y_tensor)
+                loss_dev += np.array(loss_d.data)
+            loss_dev_list.append(loss_dev / dev_size)
+            print('epoch:{:d}', 'loss:{:.4f}', 'loss_mean:{}'.format(epoch, loss_sum, loss_sum/train_size))
+            print('loss_dev_mean:{:.4f}'.format(loss_dev / dev_size))
+    t1 = time.time()
+    t_slot = t1 - t0
+    print('training time:{:.4f}'.format(t_slot))
+
+    return loss_list, loss_dev_list
+
+
+def load_data(data_folder, train_size, dev_size, vocab_size):
+
+    vocab = pd.read_table(data_folder + "/vocab.wiki.txt", header=None, sep="\s+", index_col=0,
+                          names=['count', 'freq'], )
+    num_to_word = dict(enumerate(vocab.index[:vocab_size]))  # Mapping from index to word
+    word_to_num = invert_dict(num_to_word)  # Mapping from word to index
+
+    docs = load_lm_dataset(data_folder + '/wiki-train.txt')
+    S_train = docs_to_indices(docs, word_to_num, 1, 1)
+    X_train, D_train = seqs_to_lmXY(S_train)
+
+    docs = load_lm_dataset(data_folder + '/wiki-dev.txt')
+    S_dev = docs_to_indices(docs, word_to_num, 1, 1)
+    X_dev, D_dev = seqs_to_lmXY(S_dev)
+
+    X_train = X_train[:train_size]
+    D_train = D_train[:train_size]  # Targets, i.e. one-offset X_train
+    X_dev = X_dev[:dev_size]
+    D_dev = D_dev[:dev_size]
+
+
+def batchify(data, batch_size):
+    """
+    Batchify the data, input should come from X_Train[:batch_size], i.e. a list
+    of size batch_size. In the list are arrays.
+    """
+    n_batch = len(data) // batch_size  # Number of batches
+    data = data[:n_batch * batch_size]  # Discard the extra data
+    # ====Till now, data is still a list
+    tensor_data = torch.zeros()
+    for i in range(len(data)):
+        pass
+
+
+def data_to_tensor(seq, batch_size=1):
+    """
+    Input should be one sentence, e.g. X_train[0]
+    Output will be an variable tensor
+    """
+    tensor = torch.LongTensor(seq)
+    return autograd.Variable(tensor)
+
+
+# Train and report the results from final loss of the models.
+data_folder = sys.argv[1]
+train_size = 25000
+dev_size = 1000
+vocab_size = 2000
+hdim = 50
+Max_epochs = 30
+
+load_data(data_folder, train_size, dev_size, vocab_size)
 
 print('===================RNN Model:=========================')
-f.write('===================RNN Model:=========================\n')
+model = RnnTagger(vocab_size, hdim, vocab_size, vocab_size)
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr = 0.2)
 
-model_rnn = RNNTagger(vocab_size, hdim, vocab_size, vocab_size)
-loss_function_rnn = nn.NLLLoss()
-optimizer_rnn = optim.SGD(model_rnn.parameters(), lr=0.2)
+loss_rnn, loss_dev_rnn = train(model, loss_function, optimizer)
 
-
-loss_table_rnn = []
-loss_dev_rnn = []
-
-t0 = time.time()
-for epoch in range(Max_epochs):
-    loss_sum = 0
-    loss_dev = 0
-    for idx in range(train_size):
-        X_tensor = data_to_tensor(X_train[idx])
-        Y_tensor = data_to_tensor(D_train[idx])
-        model_rnn.zero_grad()
-        model_rnn.hidden = model_rnn.init_hidden()
-        
-        pred_scores = model_rnn(X_tensor)
-        loss = loss_function_rnn(pred_scores,Y_tensor)
-        loss.backward()
-        optimizer_rnn.step()
-        
-        loss_sum += np.array(loss.data)
-    loss_table_rnn.append(loss_sum/train_size)  
-        
-    if epoch%2==0:
-        for idx in range(dev_size):
-            X_tensor = data_to_tensor(X_dev[idx])
-            Y_tensor = data_to_tensor(D_dev[idx])
-            pred_dev = model_rnn(X_tensor)
-            loss_d = loss_function_rnn(pred_dev,Y_tensor)
-            loss_dev += np.array(loss_d.data)  
-        loss_dev_rnn.append(loss_dev/dev_size)  
-        print('epoch= ',epoch,'loss is: ',loss_sum/train_size,'dev: ',loss_dev/dev_size)
-        f.write('epoch= %s'%epoch)
-        tmp = loss_sum/train_size
-        f.write('loss is: %s\t'% tmp)
-        tmp = loss_dev/dev_size
-        f.write('dev loss is: %s\n'%tmp)
-t1 = time.time()
-t_rnn = t1-t0
-print('Total time: ',t1-t0)
-f.write('Total time: %s\n'%t_rnn)
-    
 
 print('===================LSTM Model:=========================')
-f.write('===================LSTM Model:=========================\n')
 model = LSTMTagger(vocab_size, hdim, vocab_size, vocab_size)
 loss_function = nn.NLLLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.2)
 
-loss_table = []
-loss_dev_table = []
-t0 = time.time()
-for epoch in range(Max_epochs):
-    loss_sum = 0
-    loss_dev = 0
-    for idx in range(train_size):
-        X_tensor = data_to_tensor(X_train[idx])
-        Y_tensor = data_to_tensor(D_train[idx])
-        model.zero_grad()
-        model.hidden = model.init_hidden()
-        
-        pred_scores = model(X_tensor)
-        loss = loss_function(pred_scores,Y_tensor)
-        loss.backward()
-        optimizer.step()
-        
-        loss_sum += np.array(loss.data)
-    loss_table.append(loss_sum/train_size)
-
-    if epoch%2==0:
-        for idx in range(dev_size):
-            X_tensor = data_to_tensor(X_dev[idx])
-            Y_tensor = data_to_tensor(D_dev[idx])
-            pred_dev = model(X_tensor)
-            loss_d = loss_function(pred_dev,Y_tensor)
-            loss_dev += np.array(loss_d.data)
-        loss_dev_table.append(loss_dev/dev_size)
-        print('epoch= ',epoch,'loss is: ',loss_sum/train_size,'dev: ',loss_dev/dev_size)
-        f.write('epoch= %s'%epoch)
-        tmp = loss_sum/train_size
-        f.write('loss is: %s\t'% tmp)
-        tmp = loss_dev/dev_size
-        f.write('dev loss is: %s\n'%tmp)
-
-t1 = time.time()
-t_lstm = t1-t0
-print('Total time: ',t1-t0)
-f.write('Total time: %s\n'%t_lstm)
-
-
-
+loss_lstm, loss_dev_lstm = train(model, loss_function, optimizer)
 
 
 print('===================GRU Model:=========================')
-f.write('===================GRU Model:=========================\n')
-model_gru = GRUTagger(vocab_size, hdim, vocab_size, vocab_size)
-loss_function_gru = nn.NLLLoss()
-optimizer_gru = optim.SGD(model_gru.parameters(), lr=0.2)
+model = GRUTagger(vocab_size, hdim, vocab_size, vocab_size)
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.2)
 
-loss_table_gru = []
-loss_dev_gru=[]
-t0 = time.time()
-for epoch in range(Max_epochs):
-    loss_sum = 0
-    loss_dev = 0
-    for idx in range(train_size):
-        X_tensor = data_to_tensor(X_train[idx])
-        Y_tensor = data_to_tensor(D_train[idx])
-        model_gru.zero_grad()
-        model_gru.hidden = model_gru.init_hidden()
-        
-        pred_scores = model_gru(X_tensor)
-        loss = loss_function_gru(pred_scores,Y_tensor)
-        loss.backward()
-        optimizer_gru.step()
-        
-        loss_sum += np.array(loss.data)
-    loss_table_gru.append(loss_sum/train_size)
-
-    if epoch%2==0:
-        for idx in range(dev_size):
-            X_tensor = data_to_tensor(X_dev[idx])
-            Y_tensor = data_to_tensor(D_dev[idx])
-            pred_dev = model_gru(X_tensor)
-            loss_d = loss_function_gru(pred_dev,Y_tensor)
-            loss_dev += np.array(loss_d.data)   
-        loss_dev_gru.append(loss_dev/dev_size)
-        print('epoch= ',epoch,'loss is: ',loss_sum/train_size,'dev: ',loss_dev/dev_size)
-        f.write('epoch= %s'%epoch)
-        tmp = loss_sum/train_size
-        f.write('loss is: %s\t'% tmp)
-        tmp = loss_dev/dev_size
-        f.write('dev loss is: %s\n'%tmp)
-
-t1 = time.time()
-t_gru = t1-t0
-print('Total time: ',t1-t0)
-f.write('Total time: %s\n'%t_gru)
-
-f.close()
+loss_gru, loss_dev_gru = train(model, loss_function, optimizer)
 
 
-'''
-x_range = np.arange(0,15)*2
-fig1 = plt.figure(figsize=(8, 3))
-plt.subplot(1,2,1)
+# Plot the loss_curve of these 3 models
+x_range = np.arange(0, 15) * 2
+fig = plt.figure(figsize=(8, 3))
+plt.subplot(1, 2, 1)
 plt.title('Training Loss of RNN, LSTM, and GRU')
-plt.plot(loss_table_rnn,'g+-',label='RNN')
-plt.plot(loss_table,'rx-',label='LSTM')
-plt.plot(loss_table_gru,'b.-',label='GRU')
+plt.plot(loss_rnn,'g+-',label='RNN')
+plt.plot(loss_lstm,'rx-',label='LSTM')
+plt.plot(loss_gru,'b.-',label='GRU')
 plt.legend()
-plt.xlabel('No. of epochs.')
-plt.ylabel('Loss value')
+plt.xlabel('Number of Epochs')
+plt.ylabel('Loss Value')
 plt.grid(True)
 
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 plt.title('Dev Loss of RNN, LSTM, and GRU')
 plt.plot(x_range,loss_dev_rnn,'g+-',label='RNN')
-plt.plot(x_range,loss_dev_table,'rx-',label='LSTM')
+plt.plot(x_range,loss_dev_lstm,'rx-',label='LSTM')
 plt.plot(x_range,loss_dev_gru,'b.-',label='GRU')
 plt.legend()
-plt.xlabel('No. of epochs.')
-plt.ylabel('Loss value')
+plt.xlabel('Number of Epochs')
+plt.ylabel('Loss Value')
 plt.grid(True)
 
 fig1.tight_layout()
 fig1.savefig('rnn_lstm_gru.pdf')
-'''
+
 
 
 
